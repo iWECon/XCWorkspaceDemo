3e21772cfbd5afa483974605a5c630b39c191b64
iWw <i.1214@yahoo.com>
2018-06-23 01:36:27 +0800

    Upload

---
+++ AppDemo/AppDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AppDemo
+//
+//  Created by iWw on 2018/6/23.
+//  Copyright © 2018 iWw. All rights reserved.
+//
+
+import UIKit
+import StaticDemo
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        StaticDemo.showHomepage()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StaticDemo/StaticDemo/StaticDemo.swift
@@ -0,0 +1,15 @@
+//
+//  StaticDemo.swift
+//  StaticDemo
+//
+//  Created by iWw on 2018/6/23.
+//  Copyright © 2018 iWw. All rights reserved.
+//
+
+public class StaticDemo {
+    
+    public class func showHomepage() {
+        print("author: https://github.com/iWECon")
+    }
+    
+}
